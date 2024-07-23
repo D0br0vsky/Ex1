8aff39fac8cb9fe0414f9ee14794957c92b44620
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-07-23 16:20:50 +0300

    Initial Commit

---
+++ Ex1/Ex1App.swift
@@ -0,0 +1,17 @@
+//
+//  Ex1App.swift
+//  Ex1
+//
+//  Created by Dobrovsky on 23.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Ex1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
